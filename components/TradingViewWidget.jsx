c04d604d23b658d840fbfd26fd0b7f1a637cb056
Ghavio Rizky <ghavio1516@gmail.com>
2025-08-21 23:19:12 +0700

    Refactor TradingViewWidget to accept symbol prop and update widget configuration

--- components/TradingViewWidget.jsx
+++ components/TradingViewWidget.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, memo } from 'react';
 
-function TradingViewWidget() {
+function TradingViewWidget({ symbol }) {
   const container = useRef();
 
   useEffect(() => {
@@ -11,25 +11,26 @@ function TradingViewWidget() {
     script.innerHTML = `
       {
         "colorTheme": "dark",
-        "displayMode": "single",
-        "isTransparent": false,
+        "displayMode": "multiple",
+        "isTransparent": true,
         "locale": "en",
         "interval": "1m",
         "disableInterval": false,
-        "width": 425,
-        "height": 450,
-        "symbol": "NASDAQ:AAPL",
+        "width": "100%",
+        "height": "100%",
+        "symbol": "${symbol}",
         "showIntervalTabs": true
       }`;
     container.current.appendChild(script);
-  }, []);
+  }, [symbol]);  // Re-render the widget when the symbol changes
 
   return (
     <div className="tradingview-widget-container" ref={container}>
       <div className="tradingview-widget-container__widget"></div>
       <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/technicals/?exchange=NASDAQ" rel="noopener nofollow" target="_blank">
-          <span className="blue-text">Technical analysis for AAPL by TradingView</span>
+        <a href={`https://www.tradingview.com/symbols/${symbol}/technicals/`} 
+           rel="noopener nofollow" target="_blank">
+          <span className="blue-text">Technical analysis for {symbol} by TradingView</span>
         </a>
       </div>
     </div>
